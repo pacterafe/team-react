1284f7f58578c6ec281707eab23e15fad71cf54f
pacterafe <583869553@qq.com>
2019-05-08 16:17:56 +0800

    CHANGE INITIAL STRUCTURE

---
+++ src/study-demo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Components() {
+    return (
+        <section>
+            <p style={{color: '#f00',fontSize: '28px'}}>Hello Pactera FE~</p>
+        </section>
+    )
+}
+
+export default Components
