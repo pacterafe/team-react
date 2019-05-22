ed9bd8921784ef03009abc6172111d2ae797f84b
liuzhen <Lzhen198917@163.com>
2019-05-22 16:38:16 +0800

    test

--- src/study-demo/passByValue/combination/index.js
+++ src/study-demo/passByValue/combination/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 //组合combination 案例
-//这使得别的组件可以通过 JSX 嵌套，将任意组件作为子组件传给他们
+//这使得别的组件可以通过 JSX 嵌套，将任意组件作为子组件传给他们。
 function FancyBorder(props) {
     return (
       <div className={'FancyBorder FancyBorder-' + props.color}>
