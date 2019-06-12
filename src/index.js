e2ab1fa78c589851bbadc4f5444c456b5bb56b21
liuzhenl <Lzhen198917@163.com>
2019-06-12 11:01:25 +0800

    inedex

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 
-import App from './study-demo/context/contextDemo';
+import App from './study-demo/jsx/';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
