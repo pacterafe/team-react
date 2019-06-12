04c72568070349ac9a56aa0b98059d020ff4d2cc
liuzhenl <Lzhen198917@163.com>
2019-06-12 09:41:47 +0800

    changedir

--- src/study-demo/passByValue/RouterDemo/router.js
+++ src/study-demo/passByValue/RouterDemo/router.js
@@ -568,78 +568,231 @@
 
 // export default RecursiveExample;
 
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-//每个逻辑“路由”有两个组件，一个用于侧边栏，一个用于主区域。
-//当路径与当前URL匹配时，我们希望在不同的位置呈现这两个文件。
-const routes = [
-  {
-    path: "/",
-    exact: true,
-    sidebar: () => <div>home!</div>,
-    main: () => <h2>Home</h2>
-  },
-  {
-    path: "/bubblegum",
-    sidebar: () => <div>bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>
-  },
-  {
-    path: "/shoelaces",
-    sidebar: () => <div>shoelaces!</div>,
-    main: () => <h2>Shoelaces</h2>
-  }
-];
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// //每个逻辑“路由”有两个组件，一个用于侧边栏，一个用于主区域。
+// //当路径与当前URL匹配时，我们希望在不同的位置呈现这两个文件。
+// const routes = [
+//   {
+//     path: "/",
+//     exact: true,
+//     sidebar: () => <div>home!</div>,
+//     main: () => <h2>Home</h2>
+//   },
+//   {
+//     path: "/bubblegum",
+//     sidebar: () => <div>bubblegum!</div>,
+//     main: () => <h2>Bubblegum</h2>
+//   },
+//   {
+//     path: "/shoelaces",
+//     sidebar: () => <div>shoelaces!</div>,
+//     main: () => <h2>Shoelaces</h2>
+//   }
+// ];
 
-function SidebarExample() {
+// function SidebarExample() {
+//   return (
+//     <Router>
+//       <div style={{ display: "flex" }}>
+//         <div
+//           style={{
+//             padding: "10px",
+//             width: "40%",
+//             background: "#f0f0f0"
+//           }}
+//         >
+//           <ul style={{ listStyleType: "none", padding: 0 }}>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/bubblegum">Bubblegum</Link>
+//             </li>
+//             <li>
+//               <Link to="/shoelaces">Shoelaces</Link>
+//             </li>
+//           </ul>
+
+//           {routes.map((route, index) => (
+//             // 您可以在应用程序中任意位置呈现<route>。它将与任何其他与该URL匹配的<route>s一起呈现。
+//             // 因此，侧边栏或面包屑或任何其他要求您在同一个URL的多个位置呈现多个内容的东西，只不过是多个<route>s。
+//             <Route
+//               key={index}
+//               path={route.path}
+//               exact={route.exact}
+//               component={route.sidebar}
+//             />
+//           ))}
+//         </div>
+
+//         <div style={{ flex: 1, padding: "10px" }}>
+//           {routes.map((route, index) => (
+//           //渲染更多路径与以上，但这次组件不同。
+//             <Route
+//               key={index}
+//               path={route.path}
+//               exact={route.exact}
+//               component={route.main}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default SidebarExample;
+
+
+
+
+import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import "./website/modules/examples/Animation/styles.css";
+
+function AnimationExample() {
   return (
     <Router>
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            padding: "10px",
-            width: "40%",
-            background: "#f0f0f0"
-          }}
-        >
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/bubblegum">Bubblegum</Link>
-            </li>
-            <li>
-              <Link to="/shoelaces">Shoelaces</Link>
-            </li>
-          </ul>
-
-          {routes.map((route, index) => (
-            // 您可以在应用程序中任意位置呈现<route>。它将与任何其他与该URL匹配的<route>s一起呈现。
-            // 因此，侧边栏或面包屑或任何其他要求您在同一个URL的多个位置呈现多个内容的东西，只不过是多个<route>s。
+      <Route
+        render={({ location }) => (
+          <div style={styles.fill}>
             <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.sidebar}
+              exact
+              path="/"
+              render={() => <Redirect to="/hsl/10/90/50" />}
             />
-          ))}
-        </div>
 
-        <div style={{ flex: 1, padding: "10px" }}>
-          {routes.map((route, index) => (
-          //渲染更多路径与以上，但这次组件不同。
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.main}
-            />
-          ))}
-        </div>
-      </div>
+            <ul style={styles.nav}>
+              <NavLink to="/hsl/10/90/50">Red</NavLink>
+              <NavLink to="/hsl/120/100/40">Green</NavLink>
+              <NavLink to="/rgb/33/150/243">Blue</NavLink>
+              <NavLink to="/rgb/240/98/146">Pink</NavLink>
+            </ul>
+
+            <div style={styles.content}>
+              <TransitionGroup>
+                {/* no different than other usage of
+                CSSTransition, just make sure to pass
+                `location` to `Switch` so it can match
+                the old location as it animates out
+            */}
+                <CSSTransition
+                  key={location.key}
+                  classNames="fade"
+                  timeout={300}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/hsl/:h/:s/:l" component={HSL} />
+                    <Route exact path="/rgb/:r/:g/:b" component={RGB} />
+                    {/* Without this `Route`, we would get errors during
+                    the initial transition from `/` to `/hsl/10/90/50`
+                */}
+                    <Route render={() => <div>Not Found</div>} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            </div>
+          </div>
+        )}
+      />
     </Router>
   );
 }
 
-export default SidebarExample;
+function NavLink(props) {
+  return (
+    <li style={styles.navItem}>
+      <Link {...props} style={{ color: "inherit" }} />
+    </li>
+  );
+}
+
+function HSL({ match: { params } }) {
+  return (
+    <div
+      style={{
+        ...styles.fill,
+        ...styles.hsl,
+        background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
+      }}
+    >
+      hsl(
+      {params.h}, {params.s}
+      %, {params.l}
+      %)
+    </div>
+  );
+}
+
+function RGB({ match: { params } }) {
+  return (
+    <div
+      style={{
+        ...styles.fill,
+        ...styles.rgb,
+        background: `rgb(${params.r}, ${params.g}, ${params.b})`
+      }}
+    >
+      rgb(
+      {params.r}, {params.g}, {params.b})
+    </div>
+  );
+}
+
+const styles = {};
+
+styles.fill = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0
+};
+
+styles.content = {
+  ...styles.fill,
+  top: "40px",
+  textAlign: "center"
+};
+
+styles.nav = {
+  padding: 0,
+  margin: 0,
+  position: "absolute",
+  top: 0,
+  height: "40px",
+  width: "100%",
+  display: "flex"
+};
+
+styles.navItem = {
+  textAlign: "center",
+  flex: 1,
+  listStyleType: "none",
+  padding: "10px"
+};
+
+styles.hsl = {
+  ...styles.fill,
+  color: "white",
+  paddingTop: "20px",
+  fontSize: "30px"
+};
+
+styles.rgb = {
+  ...styles.fill,
+  color: "white",
+  paddingTop: "20px",
+  fontSize: "30px"
+};
+
+export default AnimationExample;
