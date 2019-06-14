f75abd470afccb42d3c29f0793df677e97677c47
liuzhenl <Lzhen198917@163.com>
2019-06-14 10:10:40 +0800

    routerDemo

--- src/study-demo/passByValue/RouterDemo/router.js
+++ src/study-demo/passByValue/RouterDemo/router.js
@@ -657,8 +657,8 @@
 //   Redirect
 // } from "react-router-dom";
 
-// import "./website/modules/examples/Animation/styles.css";
-
+// import "../Animation/styles.css";
+//动画使用案例  import { TransitionGroup, CSSTransition } from "react-transition-group";这个要npm install 
 // function AnimationExample() {
 //   return (
 //     <Router>
@@ -680,11 +680,9 @@
 
 //             <div style={styles.content}>
 //               <TransitionGroup>
-//                 {/* no different than other usage of
-//                 CSSTransition, just make sure to pass
-//                 `location` to `Switch` so it can match
-//                 the old location as it animates out
-//             */}
+//                 {/* 与csstranstation的其他用法没有区别，
+//                 只需确保将“location”传递给“switch”，
+//                 以便在动画输出时与旧位置相匹配。*/}
 //                 <CSSTransition
 //                   key={location.key}
 //                   classNames="fade"
@@ -693,9 +691,7 @@
 //                   <Switch location={location}>
 //                     <Route exact path="/hsl/:h/:s/:l" component={HSL} />
 //                     <Route exact path="/rgb/:r/:g/:b" component={RGB} />
-//                     {/* Without this `Route`, we would get errors during
-//                     the initial transition from `/` to `/hsl/10/90/50`
-//                 */}
+//                     {/* 如果没有这个“路由”，我们将在从`/`到`/hsl/10/90/50的初始转换过程中出错。`*/}
 //                     <Route render={() => <div>Not Found</div>} />
 //                   </Switch>
 //                 </CSSTransition>
@@ -795,4 +791,569 @@
 //   fontSize: "30px"
 // };
 
-// export default AnimationExample;
+// //export default AnimationExample;
+
+// //自己练习写的上边的示例；基本上是抄的
+// function RoutenDemo(){
+//     return (
+//         <Router>
+//             <Route render={({location}) => (
+//                 <div style={styles.fill}>
+//                     <Route 
+//                         exact 
+//                         path="/" 
+//                         render={() => <Redirect to="/hsl/120/100/40"/>}
+//                     />
+//                     <ul style={styles.nav}>
+//                         <NavLink to="/hsl/10/90/50">Red</NavLink>
+//                         <NavLink to="/hsl/120/100/40">Green</NavLink>
+//                         <NavLink to="/rgb/33/150/243">Blue</NavLink>
+//                         <NavLink to="/rgb/240/98/146">Pink</NavLink>
+//                     </ul>  
+//                     <div style={styles.content}>
+//                         <TransitionGroup>
+//                             <CSSTransition
+//                                 key={location.key}
+//                                 classNames="fade"
+//                                 timeout={300}
+//                             >
+//                                 <Switch location={location}>
+//                                     <Route exact path="/hsl/:h/:s/:l" component={HSL} />
+//                                     <Route exact path="/rgb/:r/:g/:b" component={RGB} />
+//                                     <Route render={() => <div>Not Found</div>} />
+//                                 </Switch>
+                                
+//                             </CSSTransition>
+//                         </TransitionGroup>
+//                     </div>
+//                 </div>
+//             )} />
+//         </Router>
+//     )
+// }
+// export default RoutenDemo
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+// function AmbiguousExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/about">About Us (static)</Link>
+//           </li>
+//           <li>
+//             <Link to="/company">Company (static)</Link>
+//           </li>
+//           <li>
+//             <Link to="/kim">Kim (dynamic)</Link>
+//           </li>
+//           <li>
+//             <Link to="/chris">Chris (dynamic)</Link>
+//           </li>
+//         </ul>
+
+//         {/*
+//             <Switch>标签
+
+//             有时您希望有静态路径的白名单喜欢“关于”和“公司”，但也允许动态
+//             像“/：用户”这样的模式。问题是“关于”不明确，将与“/about”和“/:user”匹配。
+//             大多数路由器都有一个算法来决定什么它会匹配的，因为它们只允许你匹配一个
+//             “路线”。React路由器让您可以在多个地方进行匹配故意（侧边栏、面包屑等）。所以，当你
+//             想要清除任何不明确的匹配，而不是匹配“/about”to“/:user”，只需将您的<route>s包装在
+//             <Switch>。它将呈现第一个匹配的
+//       */}
+//         <Switch>
+//           <Route path="/about" component={About} />
+//           <Route path="/company" component={Company} />
+//           <Route path="/:user" component={User} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Company() {
+//   return <h2>Company</h2>;
+// }
+
+// function User({ match }) {
+//   return (
+//     <div>
+//       <h2>User: {match.params.user}</h2>
+//     </div>
+//   );
+// }
+
+// export default AmbiguousExample;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// //有些人在集中式路由配置中找到值。
+// //路由配置只是数据。React擅长绘制地图
+// // data into components, and <Route> is a component.
+
+// ////////////////////////////////////////////////////////////
+// // 首先我们的路线组成部分
+// function Sandwiches() {
+//   return <h2>Sandwiches</h2>;
+// }
+
+// function Tacos({ routes }) {
+//   return (
+//     <div>
+//       <h2>Tacos</h2>
+//       <ul>
+//         <li>
+//           <Link to="/tacos/bus">Bus</Link>
+//         </li>
+//         <li>
+//           <Link to="/tacos/cart">Cart</Link>
+//         </li>
+//         <li>
+//           <Link to="/tacos/dingld">Dingld</Link>
+//         </li>
+//       </ul>
+
+//       {routes.map((route, i) => (
+//         <RouteWithSubRoutes key={i} {...route} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function Bus() {
+//   return <h3>Bus</h3>;
+// }
+
+// function Cart() {
+//   return <h3>Cart</h3>;
+// }
+// function Dingld() {
+//     return <h3>Dingld</h3>;
+// }
+// ////////////////////////////////////////////////////////////
+// // then our route config
+// const routes = [
+//   {
+//     path: "/sandwiches",
+//     component: Sandwiches
+//   },
+//   {
+//     path: "/tacos",
+//     component: Tacos,
+//     routes: [
+//       {
+//         path: "/tacos/bus",
+//         component: Bus
+//       },
+//       {
+//         path: "/tacos/cart",
+//         component: Cart
+//       },
+//       {
+//         path: "/tacos/dingld",
+//         component: Dingld
+//       }
+//     ]
+//   }
+// ];
+
+// //wrap<route>并在任何地方使用它，然后在
+// //子路由被添加到它将工作的任何路由中
+// function RouteWithSubRoutes(route) {
+//   return (
+//     <Route
+//       path={route.path}
+//       render={props => (
+//         // 通过子路线继续筑巢
+//         <route.component {...props} routes={route.routes} />
+//       )}
+//     />
+//   );
+// }
+
+// function RouteConfigExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/tacos">Tacos</Link>
+//           </li>
+//           <li>
+//             <Link to="/sandwiches">Sandwiches</Link>
+//           </li>
+//         </ul>
+
+//         {routes.map((route, i) => (
+//           <RouteWithSubRoutes key={i} {...route} />
+//         ))}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default RouteConfigExample;
+
+
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//没有深入研究
+// //此示例演示如何呈现两个不同的屏幕
+// //（或不同上下文中的同一屏幕）在同一URL上，
+// //取决于你是如何到达那里的。
+// //单击颜色并全屏显示，然后“访问
+// //gallery“，然后单击颜色。注意URL和组件
+// //和以前一样，但现在我们在模态中看到它们
+// //在旧屏幕的顶部。
+
+// class ModalSwitch extends Component {
+// //我们可以将一个位置传递给
+// //忽略路由器的当前位置并使用该位置
+// //改为prop。
+// //我们也可以使用“位置状态”来告诉应用程序用户
+// //希望在模式中转到“/img/2”，而不是作为
+// //主页面，使库在其后面可见。
+// //通常，`/img/2`与位于`/`的库不匹配。
+// //因此，要使两个屏幕都呈现，我们可以保存旧的
+// //定位并将其传递给switch，这样它会认为位置
+// //仍然是`/`即使它的`/img/2`。
+
+//   previousLocation = this.props.location;
+
+//   componentWillUpdate(nextProps) {
+//     let { location } = this.props;
+//     console.log(nextProps,'nextProps')
+//     console.log(nextProps.history.action,'nextProps.history.action')
+//     console.log(location.state,'location.state')
+//     // console.log(location.state.modal,'ocation.state.modal')
+//     // 如果props.location不是modal，则设置previousLocation
+//     if (
+//       nextProps.history.action !== "POP" &&
+//       (!location.state || !location.state.modal)
+//     ) {
+//       this.previousLocation = this.props.location;
+//     }
+//   }
+
+//   render() {
+//     let { location } = this.props;
+
+//     let isModal = !!(
+//       location.state &&
+//       location.state.modal &&
+//       this.previousLocation !== location
+//     ); // not initial render
+
+//     return (
+//       <div>
+//         <Switch location={isModal ? this.previousLocation : location}>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/gallery" component={Gallery} />
+//           <Route path="/img/:id" component={ImageView} />
+//         </Switch>
+//         {isModal ? <Route path="/img/:id" component={Modal} /> : null}
+//       </div>
+//     );
+//   }
+// }
+
+// const IMAGES = [
+//   { id: 0, title: "Dark Orchid", color: "DarkOrchid" },
+//   { id: 1, title: "Lime Green", color: "LimeGreen" },
+//   { id: 2, title: "Tomato", color: "Tomato" },
+//   { id: 3, title: "Seven Ate Nine", color: "#789" },
+//   { id: 4, title: "Crimson", color: "Crimson" }
+// ];
+
+// function Thumbnail({ color }) {
+//   return (
+//     <div
+//       style={{
+//         width: 50,
+//         height: 50,
+//         background: color
+//       }}
+//     />
+//   );
+// }
+
+// function Image({ color }) {
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         height: 400,
+//         background: color
+//       }}
+//     />
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div>
+//       <Link to="/gallery">Visit the Gallery</Link>
+//       <h2>Featured Images</h2>
+//       <ul>
+//         <li>
+//           <Link to="/img/2">Tomato</Link>
+//         </li>
+//         <li>
+//           <Link to="/img/4">Crimson</Link>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function Gallery() {
+//   return (
+//     <div>
+//       {IMAGES.map(i => (
+//         <Link
+//           key={i.id}
+//           to={{
+//             pathname: `/img/${i.id}`,
+//             // this is the trick!
+//             state: { modal: true }
+//           }}
+//         >
+//           <Thumbnail color={i.color} />
+//           <p>{i.title}</p>
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// }
+
+// function ImageView({ match }) {
+//   let image = IMAGES[parseInt(match.params.id, 10)];
+
+//   if (!image) return <div>Image not found</div>;
+
+//   return (
+//     <div>
+//       <h1>{image.title}</h1>
+//       <Image color={image.color} />
+//     </div>
+//   );
+// }
+
+// function Modal({ match, history }) {
+//   let image = IMAGES[parseInt(match.params.id, 10)];
+
+//   if (!image) return null;
+
+//   let back = e => {
+//     e.stopPropagation();
+//     history.goBack();
+//   };
+
+//   return (
+//     <div
+//       onClick={back}
+//       style={{
+//         position: "absolute",
+//         top: 0,
+//         left: 0,
+//         bottom: 0,
+//         right: 0,
+//         background: "rgba(0, 0, 0, 0.15)"
+//       }}
+//     >
+//       <div
+//         className="modal"
+//         style={{
+//           position: "absolute",
+//           background: "#fff",
+//           top: 25,
+//           left: "10%",
+//           right: "10%",
+//           padding: 15,
+//           border: "2px solid #444"
+//         }}
+//       >
+//         <h1>{image.title}</h1>
+//         <Image color={image.color} />
+//         <button type="button" onClick={back}>
+//           Close
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function ModalGallery() {
+//   return (
+//     <Router>
+//       <Route component={ModalSwitch} />
+//     </Router>
+//   );
+// }
+
+// export default ModalGallery;
+
+
+// import React, { Component } from "react";
+// import { StaticRouter, Route } from "react-router-dom";
+// //没看
+// // This example renders a route within a StaticRouter and populates its
+// // staticContext, which it then prints out. In the real world you would
+// // use the StaticRouter for server-side rendering:
+// //
+// // import express from "express";
+// // import ReactDOMServer from "react-dom/server";
+// //
+// // const app = express()
+// //
+// // app.get('*', (req, res) => {
+// //   let staticContext = {}
+// //
+// //   let html = ReactDOMServer.renderToString(
+// //     <StaticRouter location={req.url} context={staticContext}>
+// //       <App /> (includes the RouteStatus component below e.g. for 404 errors)
+// //     </StaticRouter>
+// //   );
+// //
+// //   res.status(staticContext.statusCode || 200).send(html);
+// // });
+// //
+// // app.listen(process.env.PORT || 3000);
+
+// function RouteStatus(props) {
+//   return (
+//     <Route
+//       render={({ staticContext }) => {
+//         // we have to check if staticContext exists
+//         // because it will be undefined if rendered through a BrowserRouter
+//         if (staticContext) {
+//           staticContext.statusCode = props.statusCode;
+//         }
+
+//         return <div>{props.children}</div>;
+//       }}
+//     />
+//   );
+// }
+
+// function PrintContext(props) {
+//   return <p>Static context: {JSON.stringify(props.staticContext)}</p>;
+// }
+
+// class StaticRouterExample extends Component {
+//   // This is the context object that we pass to the StaticRouter.
+//   // It can be modified by routes to provide additional information
+//   // for the server-side render
+//   staticContext = {};
+
+//   render() {
+//     return (
+//       <StaticRouter location="/foo" context={this.staticContext}>
+//         <div>
+//           <RouteStatus statusCode={404}>
+//             <p>Route with statusCode 404</p>
+//             <PrintContext staticContext={this.staticContext} />
+//           </RouteStatus>
+//         </div>
+//       </StaticRouter>
+//     );
+//   }
+// }
+
+// export default StaticRouterExample;
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//查询参数，很简单
+function ParamsDemo({ location }) {
+  let params = new URLSearchParams(location.search);
+
+  return (
+    <div>
+      <p>
+        React Router does not have any opinions about how your parse URL query
+        strings. Some applications use simple key=value query strings, but
+        others embed arrays and objects in the query string. So it's up to you
+        to parse the search string yourself.
+      </p>
+      <p>
+        In modern browsers that support{" "}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/API/URL">
+          the URL API
+        </a>
+        , you can instantiate a <code>URLSearchParams</code> object from{" "}
+        <code>location.search</code> and use that.
+      </p>
+      <p>
+        In{" "}
+        <a href="https://caniuse.com/#feat=url">
+          browsers that do not support the URL API (read: IE)
+        </a>{" "}
+        you can use a 3rd party library such as{" "}
+        <a href="https://github.com/sindresorhus/query-string">query-string</a>.
+      </p>
+      <div>
+        <h2>Accounts</h2>
+        <ul>
+          <li>
+            <Link to={{ pathname: "/account", search: "?name=netflix" }}>
+              Netflix
+            </Link>
+          </li>
+          <li>
+            <Link to={{ pathname: "/account", search: "?name=zillow-group" }}>
+              Zillow Group
+            </Link>
+          </li>
+          <li>
+            <Link to={{ pathname: "/account", search: "?name=yahoo" }}>
+              Yahoo
+            </Link>
+          </li>
+          <li>
+            <Link to={{ pathname: "/account", search: "?name=modus-create" }}>
+              Modus Create
+            </Link>
+          </li>
+        </ul>
+
+        <Child name={params.get("name")} />
+      </div>
+    </div>
+  );
+}
+
+function Child({ name }) {
+  return (
+    <div>
+      {name ? (
+        <h3>
+          The <code>name</code> in the query string is "{name}"
+        </h3>
+      ) : (
+        <h3>There is no name in the query string</h3>
+      )}
+    </div>
+  );
+}
+
+function ParamsExample() {
+  return (
+    <Router>
+      <Route component={ParamsDemo} />
+    </Router>
+  );
+}
+
+export default ParamsExample;
